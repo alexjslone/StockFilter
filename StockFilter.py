ca86e6dd13c8536c18ee3ec6ca980e50ac5e79ee
Alex  Slone <alexjslone12@gmail.com>
2024-04-28 16:46:30 -0700

    trying out a new approach, working from a csv of stocks

--- StockFilter.py
+++ StockFilter.py
@@ -1,39 +1,7 @@
-#from chatgpt
-#test
-import yfinance as yf
-import matplotlib.pyplot as plt
+import pandas as pd
 
-def fetch_stock_data(symbol, start_date, end_date):
-    stock_data = yf.download(symbol, start=start_date, end=end_date)
-    return stock_data
+nasdaq = pd.read_csv("nasdaq_stocks_list.csv")
 
-def filter_stock_data(stock_data, percentage_drop_threshold):
-    last_year_data = stock_data['Adj Close'].pct_change(252) * 100
-    filtered_stocks = last_year_data[last_year_data <= -percentage_drop_threshold]
-    return filtered_stocks
+us_companies = nasdaq[nasdaq['Country'] == "United States"].reset_index(drop=True)
 
-def visualize_stocks(stock_data):
-    stock_data.plot(y='Adj Close', title='Stock Prices')
-    plt.show()
-
-if __name__ == "__main__":
-    all_tickers = yf.download('')
-    print(all_tickers)
-    all_data = all_tickers.history(start='01-01-2023')
-    print(all_data)
-    #Need to figure out why it's giving me a no objects to concatenate error? 
-    """
-    symbol = 'ABNB'  # Example stock symbol (Airbnb.)
-    start_date = '2023-01-01'
-    end_date = '2024-3-7'
-    percentage_drop_threshold = 25
-
-    # Fetch stock data
-    data = fetch_stock_data(symbol, start_date, end_date)
-
-    # Filter stocks
-    filtered_stocks = filter_stock_data(data, percentage_drop_threshold)
-
-    # Visualize
-    visualize_stocks(data)
-    """
+symbols = us_companies["Symbol"].str.lower().to_list()
